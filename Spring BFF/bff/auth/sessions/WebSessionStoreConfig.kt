0411a300eed495d0ec525a18ca91e05f4c271fb0
Dreamstar Enterprises <39380005+dreamstar-enterprises@users.noreply.github.com>
2024-08-10 21:22:48 +0100

    Add files via upload

--- Spring BFF/bff/auth/sessions/WebSessionStoreConfig.kt
+++ Spring BFF/bff/auth/sessions/WebSessionStoreConfig.kt
@@ -1,21 +1,24 @@
-//package com.example.bff.auth.sessions
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
+package com.example.bff.auth.sessions
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.session.ReactiveMapSessionRepository
+import org.springframework.session.Session
 //import org.springframework.session.data.redis.ReactiveRedisIndexedSessionRepository
-//import org.springframework.session.web.server.session.SpringSessionWebSessionStore
-//import org.springframework.web.server.session.CookieWebSessionIdResolver
-//import org.springframework.web.server.session.DefaultWebSessionManager
-//import org.springframework.web.server.session.WebSessionManager
-//
-///**********************************************************************************************************************/
-///************************************************** SESSION CONFIGURATION *********************************************/
-///**********************************************************************************************************************/
-//
-//// see more here:
-//// https://docs.spring.io/spring-session/reference/web-session.html
-//// https://docs.spring.io/spring-session/reference/web-session.html#websession-how
-//
+import org.springframework.session.web.server.session.SpringSessionWebSessionStore
+import org.springframework.web.server.session.CookieWebSessionIdResolver
+import org.springframework.web.server.session.DefaultWebSessionManager
+import org.springframework.web.server.session.InMemoryWebSessionStore
+import org.springframework.web.server.session.WebSessionManager
+
+/**********************************************************************************************************************/
+/************************************************** SESSION CONFIGURATION *********************************************/
+/**********************************************************************************************************************/
+
+// see more here:
+// https://docs.spring.io/spring-session/reference/web-session.html
+// https://docs.spring.io/spring-session/reference/web-session.html#websession-how
+
 //@Configuration
 //internal class WebSessionStoreConfig {
 //
@@ -37,7 +40,48 @@
 //        return sessionManager
 //    }
 //}
-//
-///**********************************************************************************************************************/
-///**************************************************** END OF KOTLIN ***************************************************/
-///**********************************************************************************************************************/
+
+@Configuration
+internal class WebSessionStoreConfig {
+
+    @Bean
+    fun reactiveSessionRepository(): ReactiveMapSessionRepository {
+        // Use an in-memory map to store sessions
+        return ReactiveMapSessionRepository(mutableMapOf())
+    }
+
+    @Bean(name = ["webSessionStore"])
+    fun webSessionStore(
+        sessionRepository: ReactiveMapSessionRepository
+    ): SpringSessionWebSessionStore<out Session> {
+        return SpringSessionWebSessionStore(sessionRepository)
+    }
+
+//    @Bean(name = ["webSessionStore"])
+//    fun webSessionStore(): InMemoryWebSessionStore {
+//        // Create and return an in-memory session store
+//        return InMemoryWebSessionStore()
+//    }
+
+//    @Bean(name = ["webSessionManager"])
+//    fun webSessionManager(
+//        cookieWebSessionIdResolver: CookieWebSessionIdResolver,
+//        webSessionStore: SpringSessionWebSessionStore<out Session>
+//    ): WebSessionManager {
+//        val sessionManager = DefaultWebSessionManager()
+//        sessionManager.sessionStore = webSessionStore
+//        sessionManager.sessionIdResolver = cookieWebSessionIdResolver
+//        return sessionManager
+//    }
+
+    @Bean
+    fun webSessionManager(): WebSessionManager {
+        val sessionManager = DefaultWebSessionManager()
+        sessionManager.sessionIdResolver = CookieWebSessionIdResolver()
+        return sessionManager
+    }
+}
+
+/**********************************************************************************************************************/
+/**************************************************** END OF KOTLIN ***************************************************/
+/**********************************************************************************************************************/
