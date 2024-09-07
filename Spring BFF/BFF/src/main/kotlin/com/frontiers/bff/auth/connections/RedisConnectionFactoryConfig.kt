3b036b45c2a3a786875b75c8073a5c612b0bd617
Dreamstar Enterprises <39380005+dreamstar-enterprises@users.noreply.github.com>
2024-09-07 22:03:55 +0100

    Add files via upload

--- Spring BFF/BFF/src/main/kotlin/com/frontiers/bff/auth/connections/RedisConnectionFactoryConfig.kt
+++ Spring BFF/BFF/src/main/kotlin/com/frontiers/bff/auth/connections/RedisConnectionFactoryConfig.kt
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
@@ -46,6 +47,7 @@ internal class RedisConnectionFactoryConfig(
     private val springDataProperties: SpringDataProperties
 ) {
 
+    // reactive RedisConnectionFactory for key expiration event handling
     @Bean
     @Primary
     fun reactiveRedisConnectionFactory(): ReactiveRedisConnectionFactory {
@@ -162,6 +164,18 @@ internal class RedisConnectionFactoryConfig(
             validateConnection = false
             setShareNativeConnection(true)
         }
+    }
+
+    // non-reactive RedisConnectionFactory for key expiration event handling
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val config = RedisStandaloneConfiguration()
+        config.hostName = springDataProperties.redis.host
+        config.port = springDataProperties.redis.port
+        config.setPassword(RedisPassword.of(springDataProperties.redis.password))
+
+        // create Lettuce connection factory
+        return LettuceConnectionFactory(config)
     }
 
 }
