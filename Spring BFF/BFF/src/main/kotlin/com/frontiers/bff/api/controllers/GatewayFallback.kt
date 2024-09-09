0cce50db89aac037f45d9310e42359b6975e7530
Dreamstar Enterprises <39380005+dreamstar-enterprises@users.noreply.github.com>
2024-09-09 23:14:34 +0100

    Add files via upload

--- Spring BFF/BFF/src/main/kotlin/com/frontiers/bff/api/controllers/GatewayFallback.kt
+++ Spring BFF/BFF/src/main/kotlin/com/frontiers/bff/api/controllers/GatewayFallback.kt
@@ -1,5 +1,7 @@
 package com.frontiers.bff.api.controllers
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController
 internal class FallbackController {
 
     @GetMapping("/fallback")
-    fun fallback(): Map<String, String> {
-        return mapOf("message" to "Gateway fallback occured")
+    fun fallback(): ResponseEntity<Map<String, String>> {
+        val responseBody = mapOf(
+            "status" to "SERVICE_UNAVAILABLE",
+            "message" to "Gateway fallback occurred. The service might be temporarily unavailable."
+        )
+
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(responseBody)
     }
 }
 
